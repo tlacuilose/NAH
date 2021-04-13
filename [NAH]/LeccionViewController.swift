a0430fb8fd9ecd493bda9ff0c8ac84b7e3be677c
Jose Tlacuilo <tlacuilose@outlook.com>
2021-04-13 12:57:20 -0500

    Adds htmls to lessons info.

--- [NAH]/LeccionViewController.swift
+++ [NAH]/LeccionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LeccionViewController: UIViewController {
     @IBOutlet weak var leccionAttributed: UILabel!
+    @IBOutlet weak var leccionText: UITextView!
     
     @IBOutlet weak var vocabularioView: UIView!
     @IBOutlet weak var vocabularioTable: UITableView!
@@ -29,71 +30,20 @@ class LeccionViewController: UIViewController {
 
         // FIXME: HTML example for lections
         
-        let html = """
-        <!doctype html>
-        <html>
-            <style>
-            body {
-            font-family: Helvetica;
-            font-size: 18px;
-            color: #5A4522;
-            text-align: justify;
-            text-padding: 20px;
-            }
-            </style>
-          <body>
-              <h3>¿Por qué náhuatl clásico y no una variante en específico?</h3>
-            <p>Estudiar náhuatl clásico es  una buena aproximación al náhuatl hablado en las diversas regiones, además, este permite fijar de primera mano una estructura gramatical clara, la cual posteriormente aplicar los respectivos cambios de una variante ya no resultaría tan complicado que si lo realizaremos de forma contraria. </p>
-            
-            <p>Algunos cambios:</p>
-            <p>En la zona de Milpa Alta, en la Ciudad de México se suele cambiar la i por la e y la u por la o, en lugar de “mi nombre” (notoka) dicen (notuka), en “gracias” (tlazohkamati) dicen (tlazohkamate).</p>
-
-            <p>En ocasiones se pierden sonidos como por ejemplo:</p>
-            <ol>
-              <li>citlalin à citlali</li>
-              <li>tepetl à tepe</li>
-            </ol>
-
-            <p>En ocasiones se pueden agregar sonidos</p>
-            <ol>
-              <li>pía à piya</li>
-              <li>yei à yeyi</li>
-              <li>ihtoa à ihtohua</li>
-              <li>panoa à panohua</li>
-            </ol>
-
-            <p>Incluso puede haber cambios de sonidos por otros:</p>
-            <ol>
-              <li>ompa à umpa</li>
-              <li>kename à kenemi</li>
-              <li>atl à at</li>
-              <li>chicome à chigome</li>
-              <li>ihuan à ivan</li>
-              <li>tochtli à toxtli</li>
-              <li>xochitl à sochitl</li>
-            </ol>
-
-            <p>Ejemplo: Diferentes formas de decir escuela</p>
-            <ol>
-              <li>temachticalli</li>
-              <li>caltemachtilli</li>
-              <li>temachtiloyan</li>
-              <li>calmachtiloyan</li>
-              <li>nemachtiloyan</li>
-              <li>tlamachtilcalco</li>
-              <li>tlamachtilcalli</li>
-            </ol>
-
-            <p>Es importante no perderse en esto, pues si bien existen unos cambios, aprendiendo en náhuatl clásico realizar los cambios pertinentes dependiendo la región de la variante, no resultará complicado.</p>
-
-          </body>
-        </html>
-        """
-        let data = Data(html.utf8)
-        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil) {
-            leccionAttributed.attributedText = attributedString
-        }
         
+        let htmlURLString = "http://martinmolina.com.mx/202111/equipo5/data/htmls/Leccion\(self.leccion?.id ?? 1).html"
+        if let htmlURL = URL(string: htmlURLString) {
+            do {
+                let data = try Data(contentsOf: htmlURL)
+                if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil) {
+                    leccionText.attributedText = attributedString
+                }
+            } catch {
+                print("HTML Leccion contents could not be loaded")
+            }
+        } else {
+            print("The URL for leccion content html was bad.")
+        }
         
         
         if let url = URL(string: self.jsonURL) {
