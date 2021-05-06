a903d844e376f18c0ea5555f87c9d0cdd38e22a5
Oscar <a01657110@itesm.mx>
2021-05-06 16:39:03 -0400

    Add model and interface for fruits

--- [NAH]/ReconocimientoViewController.swift
+++ [NAH]/ReconocimientoViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ReconocimientoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
-    
+    @IBOutlet weak var predictionLabel: UILabel!
+    let translations = [
+    "Apple Braeburn": "mantsanatl",
+    "Apricot": "chauakatl",
+    "Avocado": "auakatli",
+    "Banana": "tsapalotl",
+    "Blueberry": "texokapolin",
+    "Cantaloupe 2": "ayotetl",
+    "Cherry 1": "kapolin",
+    "Clementine": "otonlalaxtli",
+    "Cocos": "kokotl",
+    "Corn": "elotl",
+    "Cucumber Ripe": "ayoxoxoukatl",
+    "Grape Blue": "xokomekatl",
+    "Grape White 4": "ueylalaxtli",
+    "Grapefruit Pink": "ueyxokotl",
+    "Grapefruit White": "ueyxokotl",
+    "Guava": "chalxokotl",
+    "Kiwi": "kiwitl",
+    "Lemon": "xoxokotl",
+    "Limes": "limaxokotl",
+    "Mandarine": "tentsonxokotl",
+    "Mango": "manilatsapotl",
+    "Orange": "ueylalaxtli",
+    "Papaya": "ochonetli",
+    "Strawberry": "asexokotl",
+    "Tomato 4": "xaltomatl",
+    "Watermelon": "ueleyatl",
+    ]
+
     private let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -41,4 +72,35 @@ class ReconocimientoViewController: UIViewController, UIImagePickerControllerDel
         self.imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         self.imagePicker.dismiss(animated: true, completion: nil)
     }
-}
+    
+    @available(iOS 12.0, *)
+    @IBAction func ejecutarML() {
+        //instanciar el modelo de la red neuronal
+        let modelFile = FruitClassifier1()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        //Convertir la imagen obtenida a CIImage
+        let imagenCI = CIImage(image: imageView.image!)
+        //Crear un controlador para el manejo de la imagen, este es un requerimiento para ejecutar la solicitud del modelo
+        let handler = VNImageRequestHandler(ciImage: imagenCI!)
+        //Crear una solicitud al modelo para el análisis de la imagen
+        let request = VNCoreMLRequest(model: model, completionHandler: resultadosModelo)
+        try! handler.perform([request])
+        
+    }
+    
+    func resultadosModelo(request: VNRequest, error: Error?)
+    {
+        guard let results = request.results as? [VNClassificationObservation] else { fatalError("No hubo respuesta del modelo ML")}
+        var bestPrediction = ""
+        var bestConfidence: VNConfidence = 0
+        //recorrer todas las respuestas en búsqueda del mejor resultado
+        for classification in results{
+            if (classification.confidence > bestConfidence){
+                bestConfidence = classification.confidence
+                bestPrediction = classification.identifier
+            }
+        }
+        let resultado = bestPrediction+" "+String(bestConfidence)
+        print(resultado)
+        predictionLabel.text = translations[bestPrediction]
+    }}
